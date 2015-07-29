5593709c6a42ac8810b49bb22fb13cf4128f5c91
fanyou <youfan.noey@gmail.com>
2015-07-28 23:15:02 -0400

    fix

--- debug.h
+++ debug.h
@@ -1,6 +1,11 @@
+#ifndef MY_DEBUG_DOT_H
+#define MY_DEBUG_DOT_H 1
+
 #include <cassert>
 #include <iostream>
 using std::cout;
 using std::endl;
 #define VERIFY(X) assert(X)
 #define DEBUG(X) cout<<X<<endl
+
+#endif
